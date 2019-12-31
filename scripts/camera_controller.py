831bbee5ef8cad85e15c7e670d3c25bc90c18abd
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-12-31 21:06:20 +0000

    added ship

--- scripts/camera_controller.py
+++ scripts/camera_controller.py
@@ -11,18 +11,33 @@ class CameraController(ObjectBehaviour):
             mov_dir.x += 1
         if(Input.get_key(pygame.K_a)):
             mov_dir.x -= 1
-        if(Input.get_key(pygame.K_PAGEUP)):
+        if(Input.get_key(pygame.K_PAGEUP) or Input.get_key(pygame.K_q)):
             mov_dir.y += 1
-        if(Input.get_key(pygame.K_PAGEDOWN)):
+        if(Input.get_key(pygame.K_PAGEDOWN) or Input.get_key(pygame.K_e)):
             mov_dir.y -= 1
         if(Input.get_key(pygame.K_w)):
             mov_dir.z += 1
         if(Input.get_key(pygame.K_s)):
             mov_dir.z -= 1
 
-        # mov_dir = from_np3(rotate_vectors(self.transform.rotation, mov_dir.to_np3()))
-        # mov_dir.normalize()
+        mov_dir = from_np3(rotate_vectors(self.transform.rotation, mov_dir.to_np3()))
+        mov_dir.normalize()
         self.transform.position += mov_dir * delta_time
+
+        rot_dir = vector3()
+        if(Input.get_key(pygame.K_RIGHT)):
+            rot_dir.z -= 1
+        if(Input.get_key(pygame.K_LEFT)):
+            rot_dir.z += 1
+        if(Input.get_key(pygame.K_UP)):
+            rot_dir.x += 1
+        if(Input.get_key(pygame.K_DOWN)):
+            rot_dir.x -= 1
+
+        # rot_dir.normalize()
+        rot_dir *= delta_time
+        rot_dir = from_rotation_vector(rot_dir.to_np3())
+        self.transform.rotation *= rot_dir
 
         if(Input.get_key_down(pygame.K_p)):
             bullet = GameObject("bullet")
