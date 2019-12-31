corrected objects rotation matrix

engine.py
@@ -83,7 +83,7 @@ class Transform:
     forward = property(get_forward)
 
     def get_matrix(self):
-        return Transform.get_prs_matrix(self.position, self.rotation, self.scale)
+        return Transform.get_prs_matrix(self.position, self.rotation.inverse(), self.scale)
 
     @staticmethod
     def get_position_matrix(position):
@@ -97,16 +97,10 @@ class Transform:
     def get_rotation_matrix(rotation):
         qrot  = as_rotation_matrix(rotation)
         rotation_matrix = np.identity(4)
-        rotation_matrix[0][0] = qrot[0][0]
-        rotation_matrix[0][1] = qrot[0][1]
-        rotation_matrix[0][2] = qrot[0][2]
-        rotation_matrix[1][0] = qrot[1][0]
-        rotation_matrix[1][1] = qrot[1][1]
-        rotation_matrix[1][2] = qrot[1][2]
-        rotation_matrix[2][0] = qrot[2][0]
-        rotation_matrix[2][1] = qrot[2][1]
-        rotation_matrix[2][2] = qrot[2][2]
-        rotation_matrix[3,3] = 1
+        for i in range(3):
+            for j in range(3):
+                rotation_matrix[i][j] = qrot[i][j]
+
         return rotation_matrix
 
     @staticmethod